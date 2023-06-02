cf5c1578bdc4aa6cf39bbccc258ab86c108715a1
ginettec93 <ginettecarrizo@gmail.com>
2023-06-01 21:18:02 -0300

    se han modificando los componentes con sus arrays

--- src/App.js
+++ src/App.js
@@ -3,36 +3,36 @@ import './App.css';
 import { TaskCounter} from './task-counter';
 import { TaskSearch } from './task-search';
 import { TaskList } from './task-list';
+import { TaskItem } from './task-item';
+import { CreateTaskButton } from './task-button';
+import React from 'react';
+
+const defaultTask = [
+  {text:'Study Biology', completed: false},
+  {text:'Study Geometry', completed: true},
+  {text:'Study Anatomy', completed: false},
+  {text:'Study History', completed: false},
+  {text:'Study Psychology', completed: true}
+];
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
 
-      <TaskCounter/>
+      <TaskCounter completed={10} total={20} />
       <TaskSearch/>
 
       <TaskList>
-          <TaskItem/> 
-          <TaskItem/> 
-          <TaskItem/> 
-          <TaskItem/> 
-          <TaskItem/> 
-          <TaskItem/> 
+          {defaultTask.map(task => (<TaskItem 
+          key={task.text} 
+          text={task.text}
+          completed={task.completed} />
+          ))} 
       </TaskList>
       
-      
-    </div>
-  );
-}
-
-function TaskItem() {
-  return(
-    <li>
-      <span>V</span>
-      <p>Study History</p>
-      <span>X</span>   
-    </li>
-
+      <CreateTaskButton/>
+    
+    </React.Fragment>
   );
 }
 
