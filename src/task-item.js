49663d0d18c2a5d52be6809bfc61e9dbca14bf4d
ginettec93 <ginettecarrizo@gmail.com>
2023-06-03 16:18:02 -0300

    se ha añadido una imagen de fondo

--- src/task-item.js
+++ src/task-item.js
@@ -4,7 +4,7 @@ function TaskItem(props) {
     return(
     <li>
         <span class="text_list">
-            <button class="verified_button">V{props.completed}</button>{props.text}<button class="exit_button">X</button> 
+            <button class={`verified_button ${props.completed} && verified_button`}>V{props.completed}</button>{props.text}<button class="exit_button">X</button> 
         </span>      
     </li>
     );
