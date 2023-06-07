b25f20e4c9c00f15f4b3bb2c0ba3880e38c909d8
ginettec93 <ginettecarrizo@gmail.com>
2023-06-06 22:16:07 -0300

    se han corregido detalles

--- src/task-item.js
+++ src/task-item.js
@@ -4,9 +4,9 @@ import './task-counter.css';
 function TaskItem(props) {
     return(
     <li className="TaskItem">
-    <span className={`Icon Icon_check ${props.completed && "Icon_check_active"}`} onClick={props.onComplete}>v</span> 
+    <span className={`Icon Icon_check ${props.completed && "Icon_check_active"}`} onClick={props.onComplete}>✓</span> 
     <p className={`Text_list ${props.completed && "Text_list_completed"}`}>{props.text}</p>
-    <span className= "Icon Icon_delete" onClick={props.onDelete}>x</span>       
+    <span className= "Icon Icon_delete" onClick={props.onDelete}>X</span>       
     </li>
     );
 }
